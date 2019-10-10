9e8d9d3c090033078151f7c5a7f2ee7ada865092
rafael sanchez port <rafysanchez@hotmail.com>
2019-10-10 19:29:07 -0300

    Add files via upload

---
+++ Domain/Notificacoes/Notifica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Notificacoes
+{
+    public class Notifica
+    {
+
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> notificacoes;
+    }
+}
---
+++ Domain/Servicos/ProdutoServico.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entidades;
+using Domain.Interfaces.InterfaceServico;
+using Domain.Interfaces.Produtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Servicos
+{
+    public class ProdutoServico : IProdutoServico
+    {
+
+        private readonly InterfaceProduto _InterfaceProduto;
+
+        public ProdutoServico(InterfaceProduto InterfaceProduto)
+        {
+            _InterfaceProduto = InterfaceProduto;
+        }
+
+        public Produto AdicionarVerificarExisteNome(Produto produto)
+        {
+            var nomeExiste = _InterfaceProduto.VerificaSeprodutoExistePorNome(produto);
+
+            // Trasforma para maiusculo
+            produto.Nome = TransformaTextoParaMaiusculo(produto.Nome);
+
+            if (!nomeExiste)
+            {
+                _InterfaceProduto.Add(produto);
+            }
+            else
+            {
+                produto.notificacoes.Add(new Notificacoes.Notifica
+                {
+                    NomePropriedade = "Nome",
+                    mensagem = "Já Existe produto com este Nome"
+                });
+            }
+
+            return produto;
+        }
+
+        public string TransformaTextoParaMaiusculo(string nome)
+        {
+            return nome.ToUpper();
+        }
+    }
+}
